f80f437e75d3e8b8e6b23b74e35c6a0aa308e7d5
rkdwlss2 <rkdwlss2@gmail.com>
2022-12-11 15:17:00 +0900

    Initial commit

---
+++ src/ch01/VarEx01.java
@@ -0,0 +1,12 @@
+package ch01;
+
+public class VarEx01 {
+    public static void main(String[] args){
+        boolean b1 = true; // 박스에 1이 저장됨 참 아니면 거짓
+        boolean b2= false; // 박스 0
+        int n1 = 1000;
+        double d1 = 1000.10; // 실수 저장 가능
+
+        char c1 = '가'; // 문자는 홑 따움표 써야함
+    }
+}
